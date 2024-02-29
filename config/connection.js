223710c57db32091653b5dbc5b488f0098887b75
cbursch13 <cameron.bursch@target.com>
2024-02-29 00:12:12 -0600

    added helpers.js to handle date and time for comments, bug fixing

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,24 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
+
+// Load environment variables from .env file
 require('dotenv').config();
 
-let sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-module.exports = sequelize;
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
